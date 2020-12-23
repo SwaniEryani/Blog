bdfcff6138349aa39aa3ccc7415b6e0bdc82cae5
Silwan Aleryani <silwanaleryani@Silwans-iMac.local>
2020-12-23 16:15:19 -0500

    finsh edit and delete

--- models/article.js
+++ models/article.js
@@ -1,4 +1,9 @@
 const mongoose = require("mongoose");
+const marked = require("marked");
+const slugify = require("slugify");
+const createDomPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+const dompurify = createDomPurify(new JSDOM().window);
 
 const articleSchema = new mongoose.Schema({
   title: {
@@ -14,8 +19,26 @@ const articleSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  sanitizesdHtml: {
+    type: String,
+    required: true,
+  },
 });
+articleSchema.pre("validate", function (next) {
+  if (this.title) {
+    this.slug = slugify(this.title, { lower: true, strict: true });
+  }
 
-module.exports = mongoose.model('Article', articleSchema)
+  if(this.markdown){
+    this.sanitizesdHtml = dompurify.sanitize(marked(this.markdown));
+  }
+  next();
+});
+module.exports = mongoose.model("Article", articleSchema);
--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Article = require('./models/article');
 const articleRouter = require("./routes/articals");
+const methodOverride = require('method-override');
 const app = express();
 
 mongoose.connect("mongodb://localhost:27017/blog", {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
+mongoose.set('useCreateIndex', true);
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 app.get("/", async (req, res) => {
   const articles = await Article.find().sort({
